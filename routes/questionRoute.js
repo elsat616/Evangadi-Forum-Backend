11891b0a744ef8a1fc723324df77e4507368d02a
AlexSileshi <78498675+AlexSileshi@users.noreply.github.com>
2024-02-25 12:39:53 -0500

    removed all questions

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,11 +1,1 @@
-const express = require("express");
-const router = express.Router();
 
-//questions controllers
-const { askQuestion, allQuestions } = require("../controller/questionController");
-//all questions routes
-router.get("/all_questions", authMiddleware, allQuestions);
-
-router.post("/askQuestion", askQuestion);
-
-module.exports = router;
